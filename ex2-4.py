50f2d8dcf110beb9eb6e24b4ffdefa6f3f1081af
luckydogIsaac <sp1131215@spjh.tp.edu.tw>
2024-10-22 20:39:15 +0800

    Create ex2-4.py

---
+++ ex2-4.py
@@ -0,0 +1,15 @@
+#x = 5  #"5" , 5.0
+x =float(5)
+y = "hello"
+z = 3.14159
+print ('Hello world')
+print(x)
+print(type(x))
+print(y)
+print(type(y))
+print(z)
+print(type(z))
+myvar = "John"
+print(myvar)
+my_var = "John"
+print(my_var)
