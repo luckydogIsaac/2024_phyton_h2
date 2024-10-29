128c0d611b8ead4f45613d09ba898de08c9d3548
luckydogIsaac <sp1131215@spjh.tp.edu.tw>
2024-10-29 20:13:13 +0800

    Create ex3-5.py

---
+++ ex3-5.py
@@ -0,0 +1,12 @@
+x = 3.14159
+print(f'Hello world x = {x:.5f}' ,x)
+
+username = input("Enter username:")
+
+print("username is: "+ username)
+
+x = float(input("enter float x:"))
+print(f'Hello world x = {x:.5f}')
+
+x = int(input("Enter int x:"))
+print (f'Hello world x = {x:5d}')
