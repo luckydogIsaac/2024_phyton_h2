5fc3be0a91f9fe94bc382a8d7dad88c3b0871eb8
luckydogIsaac <sp1131215@spjh.tp.edu.tw>
2024-10-29 20:50:11 +0800

    Create ex3-6.py

---
+++ ex3-6.py
@@ -0,0 +1,17 @@
+x = 3.14159
+y = 1.12345
+print(f'Hello world x = {x:.5f}' ,x)
+print(f'Hello world x = {y}' ,x)
+
+username = input("Enter username:")
+
+print("Username is: " + username)
+
+#x = float(input("Enter float x:"))
+x = input("Enter float x:")
+print(type(x))
+x = float(input("Enter float x:"))
+print (f'Helo world x = {x:.5f}')
+
+x = int(input("Enter int x:"))
+print (f'Hello world x = {x:5d}')
