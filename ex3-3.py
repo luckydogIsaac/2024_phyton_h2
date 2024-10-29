982705f16e54aa6261d77b46a4c5804fa82d899e
luckydogIsaac <sp1131215@spjh.tp.edu.tw>
2024-10-29 20:03:43 +0800

    Create ex3-3.py

---
+++ ex3-3.py
@@ -0,0 +1,21 @@
+x = int(1)# x will be 1
+#y = int(2,8)# y will be 2
+#z = int ("3): # z will be 3
+x = 1   # x will be 1
+y = 2.8 # y will be 2
+z = "3" # z will be 3
+print(x)
+print(y)
+print(z)
+print(type(x))
+print(type(y))
+print(type(z))
+x = int(1)   # x will be 1
+y = int(2.8) # y will be 2
+z = int("3") # z will be 3
+print(x)
+print(y)
+print(z)
+print(type(x))
+print(type(y))
+print(type(z))
