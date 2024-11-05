5ba5028cc95913daad0ab962ab06d8358333ca01
luckydogIsaac <sp1131215@spjh.tp.edu.tw>
2024-11-05 20:45:13 +0800

    Create ex4-7.py

---
+++ ex4-7.py
@@ -0,0 +1,10 @@
+print("pleace imput a:")
+a = int(input())
+print("pleace imput b:")
+b = int(input())
+if a<b:
+    print("you are a shoot") 
+elif a>b: 
+    print("I hate school")
+else:
+    print("ha!ha!ha!" )
