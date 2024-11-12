0f025eff5e57d19981aa10b3325a91a51d1b7280
luckydogIsaac <sp1131215@spjh.tp.edu.tw>
2024-11-12 22:04:48 +0800

    Create hw 1-3.py

---
+++ hw 1-3.py
@@ -0,0 +1,18 @@
+print("please imput A’s age")
+x = int(input())
+
+print("please imput b’s age")
+y = int(input())
+if x > y:
+    print("a is older than b and ")  
+    print(x)
+    print("years old")
+if y > x:
+    print("b is older than a and ")  
+    print(y)
+    print("years old")
+else:
+    print("a is as young as b and")  
+    print(x, y)
+    print("years old")
+   
