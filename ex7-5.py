0ba4bd336cb2deda009e1eea013354f3d3593494
luckydogIsaac <sp1131215@spjh.tp.edu.tw>
2024-11-26 20:13:23 +0800

    Create ex7-5.py

---
+++ ex7-5.py
@@ -0,0 +1,11 @@
+# 輸入四個整數
+num1 = int(input("請輸入第一個數字: "))
+num2 = int(input("請輸入第二個數字: "))
+num3 = int(input("請輸入第三個數字: "))
+num4 = int(input("請輸入第四個數字: "))
+
+# 找出最小的數
+min_num = min(num1, num2, num3, num4)
+
+# 顯示最小的數
+print(f"最小的數是: {min_num}")
