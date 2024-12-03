9e05bef1c641fbe2bd94581364ab1fee25585819
luckydogIsaac <sp1131215@spjh.tp.edu.tw>
2024-12-03 19:31:50 +0800

    Create ex8-3.py

---
+++ ex8-3.py
@@ -0,0 +1,12 @@
+thislist = ["apple","banana","cherry"]
+print(thislist)
+
+for x in thislist:
+    print(x)
+    
+print(len(thislist))
+
+for x in range(0,len(thislist)):
+    print(x," ")
+    print(thislist[x])
+    
