f1606968622c07a14e61a5b09ed80f922cda7ce3
luckydogIsaac <sp1131215@spjh.tp.edu.tw>
2024-12-10 20:38:58 +0800

    Create ex9-5.py

---
+++ ex9-5.py
@@ -0,0 +1,14 @@
+thisdict = {
+    "brade": "Ford",
+    "madel": "Mustang",
+    "year":1964,
+    "colors": ["red", "white", "blue"]
+}
+print(thisdict)
+print(len(thisdict))
+print(thisdict["brade"])
+print(thisdict["madel"])
+print(thisdict["year"])
+print(thisdict["colors"])
+thisdict["brand"] = "BMW"
+print(thisdict)
