12b7ee5b7439bcdbaa52dfc6a5b92d2a0c1d2b41
luckydogIsaac <sp1131215@spjh.tp.edu.tw>
2024-12-10 20:28:29 +0800

    Create ex9-3.py

---
+++ ex9-3.py
@@ -0,0 +1,11 @@
+thisdict = {
+    "brade": "Ford",
+    "madel": "Mustang",
+    "year":1964,
+    "year2":1984
+}
+print(thisdict)
+print(len(thisdict))
+print(thisdict["brade"])
+print(thisdict["model"])
+print(thisdict["year"])
