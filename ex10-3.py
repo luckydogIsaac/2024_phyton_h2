f53f4b7e2109ccfe68c08e875d92ca341648c85b
luckydogIsaac <sp1131215@spjh.tp.edu.tw>
2024-12-17 19:46:09 +0800

    Create ex10-3.py

---
+++ ex10-3.py
@@ -0,0 +1,19 @@
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("John", 36)
+
+print(p1)
+#--------------------------------------------------------------
+class Person:
+  def __init__(mysillyobject, name, age):
+    mysillyobject.name = name
+    mysillyobject.age = age
+
+  def myfunc(abc):
+    print("Hello my name is " + abc.name)
+
+p1 = Person("John", 36)
+print(p1)
